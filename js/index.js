8e90bc85f431b778edf7e6df856d66933b5f0702
ademclk <ademonurcelik@gmail.com>
2022-07-03 20:56:11 +0300

    Sum rendered to user.

--- js/index.js
+++ js/index.js
@@ -4,9 +4,13 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
+// More powerful than getElementById. Does the same thing.
 let sum = firstCard + secondCard
 
 function start() {
+     sumEl.textContent = "Sum : " + sum
      if (sum < 21) {
           message = "Wanna draw another card?"
      }
