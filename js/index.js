dc033c03975c0352d70388c88e6aba3f601ade45
ademclk <ademonurcelik@gmail.com>
2022-07-03 20:03:48 +0300

    Message variable declared and assigned the appropriate value

--- js/index.js
+++ js/index.js
@@ -2,19 +2,21 @@ let firstCard = 10
 let secondCard = 7
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 let sum = firstCard + secondCard
 
 if (sum < 21) {
-     console.log("Wanna draw another card?")
+     message = "Wanna draw another card?"
 }
 else if (sum === 21) {
-     console.log("Blackjack!")
-        hasBlackJack = true
+     message = "Blackjack!"
+     hasBlackJack = true
 }
 else if (sum > 21) {
-     console.log("Bust!")
+     message = "Bust!"
      isAlive = false
 }
 
 console.log(hasBlackJack)
 console.log(isAlive)
+console.log(message)
