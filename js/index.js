770603407ad4485aa7172e832001fa667a1b39dd
ademclk <ademonurcelik@gmail.com>
2022-07-04 00:03:39 +0300

    Fixed the start function and declared the player object.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 let cards = [] // Creating an array.
+let sum = 0
+let isStarted = false
 let hasBlackJack = false
 let isAlive = false
 let message = ""
+
 let messageEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
-let sumEl = document.querySelector("#sum-el")
+let sumEl = document.getElementById("sum-el")
+//let sumEl = document.querySelector("#sum-el")
 // More powerful than getElementById. Does the same thing.
 let cardsEl = document.getElementById("cards-el")
-let sum = 0
+
+let player = {
+     name : "Adem",
+     chips : 256
+}
+
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": " + player.chips
+
 
 function getRandomCard() {
      let temp = Math.floor(Math.random() * 13) + 1
@@ -23,14 +35,17 @@ function getRandomCard() {
 }
 
 function start() {
-     console.log("Starting game")
-     isAlive = true
-     let firstCard = getRandomCard()
-     cards.push(firstCard)
-     let secondCard = getRandomCard()
-     cards.push(secondCard)
-     sum = cards[0] + cards[1]
-     render()
+     if (isStarted === false) {
+          console.log("Starting game")
+          isAlive = true
+          let firstCard = getRandomCard()
+          cards.push(firstCard)
+          let secondCard = getRandomCard()
+          cards.push(secondCard)
+          sum = cards[0] + cards[1]
+          render()
+          isStarted = true
+     }
 }
 
 function render() {
